d4ac1a9a857f686d596e8c69aca960e6a02c6290
Fabrício Nascimento Moreira <fabricionasciment77@gmail.com>
2026-03-04 07:00:57 -0300

    Finalizando SobreSection, refatorando techBox para receber props de classeName, fazendo module de da techstack e SobreSection sendo responsivos Web e Mobile em diversos tamanhos formatos

--- client/src/components/sections/sobre/TechBox.tsx
+++ client/src/components/sections/sobre/TechBox.tsx
@@ -1,11 +1,13 @@
 type TechBoxProps = {
   title: string
+  className?: string
 }
 
-export default function TechBox({ title }: TechBoxProps) {
+export default function TechBox({ title, className }: TechBoxProps) {
   return (
-    <div className="tech-box">
+    <div className={`tech-box ${className ?? ""}`}>
       <h3>{title}</h3>
     </div>
   )
 }
+
--- client/src/components/sections/sobre/TechStack.tsx
+++ client/src/components/sections/sobre/TechStack.tsx
@@ -1,16 +1,17 @@
 import TechBox from "./TechBox"
+import styles from "./TechStack.module.css"
 
 export default function TechStack() {
   return (
-    <div className="tech-stack">
-      <div className="main-techs">
-        <TechBox title="JavaScript" />
-        <TechBox title="React" />
-        <TechBox title="Node.js" />
-        <TechBox title="SQL" />
+    <div className={styles.techStack}>
+      <div className={styles.mainTechs}>
+        <TechBox title="JavaScript" className={styles.techBox} />
+        <TechBox title="React" className={styles.techBox} />
+        <TechBox title="Node.js" className={styles.techBox} />
+        <TechBox title="SQL" className={styles.techBox} />
       </div>
 
-      <p className="secondary-techs">
+      <p className={styles.secondaryTechs}>
         Git • AWS • MVC • JWT • Figma
       </p>
     </div>
